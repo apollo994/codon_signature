9e1e75e8f244f640df162786ad25320b4e1a678f
zanarfa <zanarfa@gmobiongsws.jrc.it>
2019-09-16 17:05:10 +0200

    freq counter

--- get_codon_freq.py
+++ get_codon_freq.py
@@ -7,6 +7,8 @@ import sys
 import argparse
 from collections import defaultdict
 import pandas as pd
+import numpy as np
+import collections
 
 
 def main():
@@ -15,9 +17,8 @@ def main():
     parser.add_argument('--output', metavar='OUTPUTFILE', default="/dev/stdout", help='The output file.')
     args = parser.parse_args()
 
+    #Dicrionary cointaing the genetic code (AA/correspondig triplets)
     genecode=dict()
-
-
 
     genecode["A"] = ["GCT","GCC","GCA","GCG"]
     genecode["L"] = ["TTA","TTG","CTT","CTC","CTA","CTG"]
@@ -41,55 +42,41 @@ def main():
     genecode["V"] = ["GTT","GTC","GTA","GTG"]
     genecode["Z"] = ["TAA","TGA","TAG"]
 
-    #print (genecode)
 
     data=pd.read_csv(args.input, sep="\t")
+    data=data.set_index("Assembly")
+    # data.index.names = [None]
+    ratio_all=pd.DataFrame(index=data.index)
 
-    #sub_data=data.loc[:,["Assembly"]+genecode["A"]]
-    #sub_data.set_index("Assembly")
-    sub_data=data.loc[:,genecode["A"]]
-    
-    ratio_subdata=sub_data.div(sub_data.sum(axis=1), axis=0))
+    #table with all triplets frequencies
 
-    print (data)
-    print()
-    print (sub_data)
+    for AA in genecode:
+        sub_data=data.loc[:,genecode[AA]]
+        # print(sub_data)
+        ratio_subdata=sub_data.div(sub_data.sum(axis=1), axis=0)
+        # print(ratio_subdata)
+        ratio_all=pd.merge(ratio_all,ratio_subdata,on="Assembly")
 
+    ratio_all=ratio_all.round(2)
 
-
-# #    for index, row in data.iterrows():
-# #        print(row["TTT"])
-#     testsum = data["GCT"] + data["GCC"]
-#     #print(testsum)
-#     buff=pd.DataFrame()
-#
-#
-#     for codon in genecode["A"]:
-#         buff.append((codon,list(data[codon].values)))
-#
-#     print (buff)
-#
-#     for i in [j[1] for j in buff]:
-#         print (i)
-#
-#     #print(x[1] for x in buff)
-#
-#     #print (summ_buff)
-#
-#     #    print (codon)
-#     #    for value in data[codon]:
-#     #        print (value)
-#         #print(data.loc(codon))
-#
-#
-#     #print (data.columns)
-#
-#
-#     #print (data)
+    for trip in ratio_all.columns:
+        trip_freq=list(ratio_all.loc[:,trip])
+        trip_freq=collections.Counter(trip_freq)
+        top3=trip_freq.most_common(3)
+        
 
 
 
-
+    # print (ratio_all)
+    # print ()
+    # GCT_freq=list(ratio_all.loc[:,"GCT"])
+    # print (GCT_freq)
+    # print ()
+    # counter=collections.Counter(GCT_freq)
+    # print(counter)
+    # print(counter.values())
+    # print(counter.keys())
+    # print(counter.most_common(3))
 
 
 if __name__ == "__main__":
