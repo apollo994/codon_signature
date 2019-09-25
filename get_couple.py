1e694d2b7810894320967347ddd749f217f8d5c8
apollo994 <fabio.zanarelllo.94@gmail.com>
2019-09-25 14:37:08 +0200

    couple

--- get_couple.py
+++ get_couple.py
@@ -18,7 +18,66 @@ def main():
     parser.add_argument('--output', metavar='OUTPUTFILE', default="/dev/stdout", help='The output file.')
     args = parser.parse_args()
 
-    get the freq table and make the couple
+    #code to print the couple
+
+    # codons1=["AAA","AAC","AAG","AAT","ACA","ACC","ACG","ACT","AGA","AGC","AGG",
+    # "AGT","ATA","ATC","ATG","ATT","CAA","CAC","CAG","CAT","CCA","CCC","CCG","CCT",
+    # "CGA","CGC","CGG","CGT","CTA","CTC","CTG","CTT"]
+    #
+    # codons2=["GAA","GAC","GAG","GAT","GCA","GCC","GCG","GCT","GGA","GGC","GGG",
+    # "GGT","GTA","GTC","GTG","GTT","TAA","TAC","TAG","TAT","TCA","TCC","TCG","TCT",
+    # "TGA","TGC","TGG","TGT","TTA","TTC","TTG","TTT"]
+    # codons2.reverse()
+    #
+    # for i in range(1,33):
+    #     print("couple[\"C"+str(i)+"\"]=[\""+codons1[i-1]+"\",\""+codons2[i-1]+"\"]")
+
+
+    data=pd.read_csv(args.input, sep="\t", index_col=False, low_memory=False)
+
+    couple = dict()
+
+    couple["C1"]=["AAA","TTT"]
+    couple["C2"]=["AAC","TTG"]
+    couple["C3"]=["AAG","TTC"]
+    couple["C4"]=["AAT","TTA"]
+    couple["C5"]=["ACA","TGT"]
+    couple["C6"]=["ACC","TGG"]
+    couple["C7"]=["ACG","TGC"]
+    couple["C8"]=["ACT","TGA"]
+    couple["C9"]=["AGA","TCT"]
+    couple["C10"]=["AGC","TCG"]
+    couple["C11"]=["AGG","TCC"]
+    couple["C12"]=["AGT","TCA"]
+    couple["C13"]=["ATA","TAT"]
+    couple["C14"]=["ATC","TAG"]
+    couple["C15"]=["ATG","TAC"]
+    couple["C16"]=["ATT","TAA"]
+    couple["C17"]=["CAA","GTT"]
+    couple["C18"]=["CAC","GTG"]
+    couple["C19"]=["CAG","GTC"]
+    couple["C20"]=["CAT","GTA"]
+    couple["C21"]=["CCA","GGT"]
+    couple["C22"]=["CCC","GGG"]
+    couple["C23"]=["CCG","GGC"]
+    couple["C24"]=["CCT","GGA"]
+    couple["C25"]=["CGA","GCT"]
+    couple["C26"]=["CGC","GCG"]
+    couple["C27"]=["CGG","GCC"]
+    couple["C28"]=["CGT","GCA"]
+    couple["C29"]=["CTA","GAT"]
+    couple["C30"]=["CTC","GAG"]
+    couple["C31"]=["CTG","GAC"]
+    couple["C32"]=["CTT","GAA"]
+
+    new_col=list(couple.keys())
+
+    for element in couple:
+        temp_data=data.iloc[:,couple[element]]
+    #     print (couple[element])
+    # #print (temp_data)
+    #print (data.loc[:,["AAA","TTT"]])
+
 
 if __name__ == "__main__":
     main()
