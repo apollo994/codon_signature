baf7af8c777f2e846f6aaeb1f42b486ddba0bca8
acblaumeiser <ablaumeiser@stud.uni-frankfurt.de>
2019-10-04 12:16:31 +0200

    progress_2019-10-04

--- get_final_table.py
+++ get_final_table.py
@@ -33,10 +33,12 @@ def get_column_distribution(data, limit):
         #print(freqs)
         freqs = {freq: freqs[freq]/total for freq in freqs}
         #print(freqs)
-        sorted_freqs = sorted(freqs.items(), key=operator.itemgetter(1), reverse=True)
+        sorted_freqs = sorted(freqs.items(), key=operator.itemgetter(1, 0), reverse=True)
+        
         #rel_freqs = {values[0]: values[1]/total for values in sorted_abs_freqs}
         #print(rel_freqs)
-        #print(sorted_freqs)
+        print(column_name)
+        print(sorted_freqs)
         index = 0
         max_value = sorted_freqs[index]
         percentage = max_value[1]
@@ -66,6 +68,7 @@ def get_column_distribution(data, limit):
 
 def build_table(distributions, nr):
     sample_dict = {}
+    #re-scaling to 1 (since it is supposed to be a distribution)
     #Should be moved to get_column_distribution function
     for column in distributions:
         keys = list(distributions[column].keys())
@@ -89,28 +92,24 @@ def build_table(distributions, nr):
 def main():
 
     parser = argparse.ArgumentParser(description='My nice tool.')
-    parser.add_argument('--input', metavar='INPUTFILE', default="/dev/stdin", help='The input file.')
+    parser.add_argument('-i', '--input', metavar='INPUTFILE', default="/dev/stdin", help='The input file.')
     #parser.add_argument('--id', metavar='ID',help='RefSeq tax ID',type=int)
-    parser.add_argument('--output', metavar='OUTPUTFILE', default="/dev/stdout", help='The output file.')
+    parser.add_argument('-o', '--output', metavar='OUTPUTFILE', default="/dev/stdout", help='The output file.')
     args = parser.parse_args()
 
     threshold = 0.9
-    nr_rows = 10000
-
+    nr_rows = 10
 
     inpath = args.input
-
-    #inpath = '/media/andreas/Data/jrc_codon/data/test_table.tsv'
-    #inpath = '/media/andreas/Data/jrc_codon/data/out_step2.tsv'
     outpath = args.output
-    #outpath = '/media/andreas/Data/jrc_codon/data/training_data.tsv'
+
     table = parse_table(inpath)
     distributions = get_column_distribution(table, threshold)
     #print(distributions)
     training_data = build_table(distributions, nr_rows)
     #print(training_data)
     #Write output
-    training_data.to_csv(outpath, sep='\t', index=False)
+    #training_data.to_csv(outpath, sep='\t', index=False)
     #print(list(table.columns)[2:])
     #for column in table.columns[2:]:
     #    print(table[column])
