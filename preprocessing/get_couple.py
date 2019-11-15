09dbf09af59df356c0d96bd3362f3f0e60d6ed43
Fabio Zanarello <zanarfa@ubuntu-gpu-7-200gb.elastic-comp.mgmt.ipr.adm>
2019-11-15 15:35:09 +0100

    folder prep

---
+++ preprocessing/get_couple.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# Fabio Zanarello, JRC-ispra, 2019
+
+
+import os
+import sys
+import argparse
+from collections import defaultdict
+import pandas as pd
+import numpy as np
+import collections
+
+
+def main():
+    parser = argparse.ArgumentParser(description='My nice tool.')
+    parser.add_argument('--input', metavar='INPUTFILE', default="/dev/stdin", help='The input file.')
+    #parser.add_argument('--id', metavar='ID',help='RefSeq tax ID',type=int)
+    parser.add_argument('--output', metavar='OUTPUTFILE', default="/dev/stdout", help='The output file.')
+    args = parser.parse_args()
+
+    #code to print the dictionary couple
+
+    # codons1=["AAA","AAC","AAG","AAT","ACA","ACC","ACG","ACT","AGA","AGC","AGG",
+    # "AGT","ATA","ATC","ATG","ATT","CAA","CAC","CAG","CAT","CCA","CCC","CCG","CCT",
+    # "CGA","CGC","CGG","CGT","CTA","CTC","CTG","CTT"]
+    #
+    # codons2=["GAA","GAC","GAG","GAT","GCA","GCC","GCG","GCT","GGA","GGC","GGG",
+    # "GGT","GTA","GTC","GTG","GTT","TAA","TAC","TAG","TAT","TCA","TCC","TCG","TCT",
+    # "TGA","TGC","TGG","TGT","TTA","TTC","TTG","TTT"]
+    # codons2.reverse()
+    #
+    # for i in range(1,33):
+    #     print("couple[\"C"+str(i)+"\"]=[\""+codons1[i-1]+"\",\""+codons2[i-1]+"\"]")
+
+    #read rge triplets freq table from step1
+    data=pd.read_csv(args.input, sep="\t", index_col=False, low_memory=False)
+
+    #dictionary cointainig the complementary triplets
+    couple = dict()
+    # couple["C1"]=["AAA","TTT"]
+    # couple["C2"]=["AAC","TTG"]
+    # couple["C3"]=["AAG","TTC"]
+    # couple["C4"]=["AAT","TTA"]
+    # couple["C5"]=["ACA","TGT"]
+    # couple["C6"]=["ACC","TGG"]
+    # couple["C7"]=["ACG","TGC"]
+    # couple["C8"]=["ACT","TGA"]
+    # couple["C9"]=["AGA","TCT"]
+    # couple["C10"]=["AGC","TCG"]
+    # couple["C11"]=["AGG","TCC"]
+    # couple["C12"]=["AGT","TCA"]
+    # couple["C13"]=["ATA","TAT"]
+    # couple["C14"]=["ATC","TAG"]
+    # couple["C15"]=["ATG","TAC"]
+    # couple["C16"]=["ATT","TAA"]
+    # couple["C17"]=["CAA","GTT"]
+    # couple["C18"]=["CAC","GTG"]
+    # couple["C19"]=["CAG","GTC"]
+    # couple["C20"]=["CAT","GTA"]
+    # couple["C21"]=["CCA","GGT"]
+    # couple["C22"]=["CCC","GGG"]
+    # couple["C23"]=["CCG","GGC"]
+    # couple["C24"]=["CCT","GGA"]
+    # couple["C25"]=["CGA","GCT"]
+    # couple["C26"]=["CGC","GCG"]
+    # couple["C27"]=["CGG","GCC"]
+    # couple["C28"]=["CGT","GCA"]
+    # couple["C29"]=["CTA","GAT"]
+    # couple["C30"]=["CTC","GAG"]
+    # couple["C31"]=["CTG","GAC"]
+    # couple["C32"]=["CTT","GAA"]
+
+
+    couple["C1"]=["AAG","CTT"]
+    couple["C2"]=["GTA","TAC"]
+    couple["C3"]=["ACC","GGT"]
+    couple["C4"]=["AAC","GTT"]
+    couple["C5"]=["GGA","TCC"]
+    couple["C6"]=["GCA","TGC"]
+    couple["C7"]=["AGG","CCT"]
+    couple["C8"]=["ATA","TAT"]
+    couple["C9"]=["CCA","TGG"]
+    couple["C10"]=["TAA","TTA"]
+    couple["C11"]=["ATC","GAT"]
+    couple["C12"]=["CCG","CGG"]
+    couple["C13"]=["CTC","GAG"]
+    couple["C14"]=["ACA","TGT"]
+    couple["C15"]=["AGC","GCT"]
+    couple["C16"]=["TCA","TGA"]
+    couple["C17"]=["CAA","TTG"]
+    couple["C18"]=["ACG","CGT"]
+    couple["C19"]=["AGA","TCT"]
+    couple["C20"]=["CGA","TCG"]
+    couple["C21"]=["TAG","CTA"]
+    couple["C22"]=["ATG","CAT"]
+    couple["C23"]=["GAA","TTC"]
+    couple["C24"]=["GCC","GGC"]
+    couple["C25"]=["CCC","GGG"]
+    couple["C26"]=["GAC","GTC"]
+    couple["C27"]=["CAG","CTG"]
+    couple["C28"]=["AAT","ATT"]
+    couple["C29"]=["CAC","GTG"]
+    couple["C30"]=["AAA","TTT"]
+    couple["C31"]=["CGC","GCG"]
+    couple["C32"]=["ACT","AGT"]
+
+    #list of the triplets used to create an empty DataFrame
+    new_col=list(couple.keys())
+    data_couple=pd.DataFrame(columns=new_col)
+
+    for element in couple:
+        #only two triplets of interest
+        temp_data=data[couple[element]]
+        #print (temp_data)
+        #seq name of the two triplets
+        AA1=couple[element][0]
+        AA2=couple[element][1]
+        cup_values=[]
+        #cycle on each row to eaxtract triplets fres
+        for index, row in temp_data.iterrows():
+            AA_values=[]
+            AA_values.append(row[AA1])
+            AA_values.append(row[AA2])
+            ##pseudo count (1) is added to avoid division by 0
+            #AA_values.append(row[AA1]+1)
+            #AA_values.append(row[AA2]+1)
+            #freq values with increasing order
+            AA_values.sort()
+            #ratio of the freq (alway smaller/bigger)
+            cup_values.append(AA_values[0]/AA_values[1])
+
+        #add the values to the couple column
+        data_couple[element]=cup_values
+
+
+    data_couple=data_couple.round(2)
+    data_couple.to_csv(args.output, sep="\t",index=False, )
+
+
+if __name__ == "__main__":
+    main()
---
+++ preprocessing/get_final_table.py
@@ -0,0 +1,208 @@
+#Author: Andreas Blaumeiser, Joint Research Centre F7 Unit, Ispra (Italy)
+#Date: 2019-10-09
+#TODO: move re-scaling from build_table to get_column_distribution function
+ 
+import argparse
+import fractions
+from functools import reduce
+import math
+import numpy as np
+import operator
+import pandas as pd
+
+
+#parse_table
+#Reads in the data from the input file.
+#
+#Parameters:
+#inpath: path to the input file
+#
+#Output:
+#data from the input file transformed into a Pandas dataframe
+def parse_table(inpath):
+    data = pd.read_csv(inpath, sep='\t', index_col=False, low_memory=False)
+    return data
+
+
+#get_column_distribution
+#Finds the most common values for each data column and constructs a
+#distribution.
+#
+#Parameters:
+#data: dataframe with all fraction values for the pairs of complementary codons
+#limit: the defined threshold of how much percentage of the column should be
+#       covered by the selected values
+#
+#Output:
+#dictionary with the most common values per column and the percentage of the
+#total column that these values cover
+def get_column_distribution(data, limit):
+    #the eventual output of the function
+    distributions = {}
+    #find the distribution for each column
+    for column_name in data.columns:
+        #count the absolute frequencies
+        freqs = {}
+        column_values = data[column_name].values
+        for value in column_values:
+            try:
+                freqs[value] += 1
+            except:
+                freqs[value] = 1
+        #transform absolute freqs into relative freqs
+        total = reduce(lambda x,y: x+y, freqs.values())
+        freqs = {freq: freqs[freq]/total for freq in freqs}
+        #sort the values according to the frequencies to find the maxima
+        sorted_freqs = sorted(
+            freqs.items(), key=operator.itemgetter(1), reverse=True
+        )
+        #the highest percentage value
+        max_percent = sorted_freqs[0][1]
+        #find the list of all maxima
+        maxima = [tup[0] for tup in sorted_freqs if tup[1] == max_percent]
+        #add the values of the maxima to the column distribution
+        for mx in maxima:
+            try:
+                distributions[column_name][mx] = max_percent
+            except:
+                distributions[column_name] = {mx: max_percent}
+        #check if the maxima already cover more than the threshold because then
+        #no need to continue
+        percentage = len(maxima)*max_percent
+        if percentage >= limit:
+            continue
+        #if the maxima do not cover the limit, extend the range of values
+        else:
+            #set to check which values have already been added to avoid
+            #repeated considerations of values (all maxima belong to this
+            #set by default)
+            touched_values = set(maxima)
+            #dictionary to keep track of the extension around the maxima
+            #0: left neighbor, 1: right neighbor
+            maxima_dict = {
+                maxi: {0: maxi - 0.01, 1: maxi + 0.01} for maxi in maxima
+            }
+
+            #auxiliary function to identify the next best value that should
+            #be added to the column's distribution
+            def find_next_value(ptg):
+                nbr_values = []
+                #go through neighbors of all maxima to find new best value
+                for maxi in maxima:
+                    left = maxima_dict[maxi][0]
+                    right = maxima_dict[maxi][1]
+
+                    if not left in touched_values and left in freqs:
+                        nbr_values.append(((maxi, 0), freqs[left]))
+                    if not right in touched_values and right in freqs:
+                        nbr_values.append(((maxi, 1), freqs[right]))
+
+                if nbr_values:
+                    next_step = (sorted(nbr_values, key=operator.itemgetter(1), reverse=True))[0]
+                    next_value = maxima_dict[next_step[0][0]][next_step[0][1]]
+                    next_max = next_step[1]
+                    #update list of touched values
+                    touched_values.add(next_value)
+                    #update maxima_dict
+                    curr_max = next_step[0][0]
+                    nbr = next_step[0][1]
+                    #values need to be rounded to avoid floating point issues,
+                    #e.g. 0.81+0.01=0.8200000000000001 in vanilla Python
+                    if nbr == 0:
+                        new_left = round(maxima_dict[curr_max][0] - 0.01, 2)
+                        maxima_dict[curr_max][0] = new_left
+                    elif nbr == 1:
+                        new_right = round(maxima_dict[curr_max][1] + 0.01, 2)
+                        maxima_dict[curr_max][1] = new_right
+                    return (next_value, next_max)
+                else:
+                    return False
+
+            #if the limit has not been reached yet, more values can be added
+            #to the distribution by calling the find_next_value function
+            while percentage < limit:
+                next_step = find_next_value(percentage)
+                if next_step:
+                    distributions[column_name][next_step[0]] = next_step[1]
+                    percentage = math.fsum([percentage, next_step[1]])
+                #limit cannot be reached because the next step reached only
+                #zeros (or went out of boundaries - <0 or >1), stop extension
+                else:
+                    break
+                
+    return distributions
+
+
+#build_table
+#Creates a table of values for each pair of codons column picked randomly
+#with a distribution according to all values in the column.
+#
+#Parameters:
+#distributions: dictionary with the precalculated distributions for the columns
+#nr: number of rows that the table should have
+#
+#Output:
+#Pandas dataframe with the random values for each column
+def build_table(distributions, nr):
+    sample_dict = {}
+    #re-scaling to 1 (since it is supposed to be a distribution)
+    #Should be moved to get_column_distribution function
+    for column in distributions:
+        keys = list(distributions[column].keys())
+        values = list(distributions[column].values())
+        percent = reduce(lambda x,y: x+y, values)
+        #Fraction method must be used to avoid floating point issues that will
+        #cause the re-scaled values to not add up to 1
+        norm_values = [
+            fractions.Fraction(value/percent).limit_denominator()
+            for value in values
+        ]
+
+        if reduce(lambda x,y: x+y, norm_values) != 1:
+            print('numerical issue with {}'.format(column))
+        else:
+            sample = np.random.choice(keys, p=list(norm_values), size=nr)
+            sample_dict[column] = sample
+
+    sample_df = pd.DataFrame(sample_dict)
+    return sample_df
+
+
+def main():
+    #argument parsing
+    parser = argparse.ArgumentParser(description='Codon usage sample tool.')
+    parser.add_argument(
+        '-i', '--input', metavar='INPUTFILE', default="/dev/stdin",
+        help='The input file.'
+    )
+    parser.add_argument(
+        '-o', '--output', metavar='OUTPUTFILE', default="/dev/stdout",
+        help='The output file.'
+    )
+    parser.add_argument(
+        '-r', '--rows', metavar='int', type=int, const=10000, default=10000,  
+        nargs='?', help='Number of rows for model training.'
+    )
+    parser.add_argument(
+        '-t', '--threshold', metavar='float', type=float, const=0.9,
+         help='Threshold for value inclusion.',nargs='?', default=0.9
+    )
+    args = parser.parse_args()
+    
+    #defining global parameters according to the command line arguments
+    threshold = args.threshold
+    nr_rows = args.rows
+    inpath = args.input
+    outpath = args.output
+
+    #parse input
+    table = parse_table(inpath)
+    #build the distributions for the columns
+    distributions = get_column_distribution(table, threshold)
+    #create the table with the random samples for each column
+    training_data = build_table(distributions, nr_rows)
+    #write the table as .tsv to the output file
+    training_data.to_csv(outpath, sep='\t', index=False)
+
+if __name__ == "__main__":
+    main()
